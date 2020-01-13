4fb5409448baae17b518d97710042208eb8612ea
buckn <nathanieljbuck7@gmail.com>
2020-01-13 11:35:22 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
+
+    loop {
+    	panic!("Some panic message");
+    }
+}
